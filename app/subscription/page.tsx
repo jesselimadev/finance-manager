63a428b8235c65f13f4baa9aafd74330dd774956
jesselimadev <170663907+jesselimadev@users.noreply.github.com>
2024-11-20 20:24:38 +0000

    chore: add user id property to transaction table

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,5 @@
-const SubscriptionPage = () => { 
-    return <h1>Subscription page</h1>
-  };
-  
-  export default SubscriptionPage;
+const SubscriptionPage = () => {
+  return <h1>Subscription page</h1>;
+};
+
+export default SubscriptionPage;
